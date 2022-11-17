b8c603ef9cb83789595c5aaf2b2d094cb17614c4
yuxin yang <yuxin@yuxindeMacBook-Pro.local>
2022-11-17 17:06:47 +0000

    revise

--- proj4.R
+++ proj4.R
@@ -1,118 +1,105 @@
-"Overview:
-The whole file is about an R function, newt, implementing Newton’s method for
-minimization of functions. In the program, we are mainly divided into six steps. 
-Step 1: evaluate function value, gradient and hessian matrix
-Step 2: test whether k-step theta is minimum and terminate if it is.
-Step 3: if k-step hessian matrix is not positive definite, perturb it.
-Step 4: solve H*Delta = -gradient for the search direction Delta.
-Step 5: if function value of next step is not less than this step,repeatedly 
- halve Delta until it is.
-Step 6: set k+1-step theta = k-step theta + Delta, 
- increment k by 1 and return to Step 1
-We multiply a small multiple (1e-10) of the norm of the Hessian matrix to perturb it
-And the project issue errors or warnings by try() function when the objective or 
-derivatives are not finite at the initial theta, the step fails to reduce 
-the objective, maxit is reached without convergence and the Hessian 
-is not positive definite at convergence.
-"
+#Name:
+#Address of github repo:
+#Contribution:
+#Overview:
+#The whole file is about an R function, newt, implementing Newton’s method for
+#minimization of functions. In the program, we are mainly divided into six steps. 
+#Step 1: evaluate function value, gradient and hessian matrix
+#Step 2: test whether k-step theta is minimum and terminate if it is.
+#Step 3: if k-step hessian matrix is not positive definite, perturb it.
+#Step 4: solve H*Delta = -gradient for the search direction Delta.
+#Step 5: if function value of next step is not less than this step,repeatedly 
+#  halve Delta until it is.
+#Step 6: set k+1-step theta = k-step theta + Delta, 
+#  increment k by 1 and return to Step 1
+#We multiply a small multiple (1e-10) of the norm of the Hessian matrix to perturb it
+#And the project issue errors or warnings by try() function when the objective or 
+#derivatives are not finite at the initial theta, the step fails to reduce 
+#the objective, maxit is reached without convergence and the Hessian 
+#is not positive definite at convergence.
 
 newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,max.half=20,eps=1e-6){
+  #calculate f, gradient, Hessian at the initial theta
   f <- func(theta, ...)
   gradient <- grad(theta, ...)
   H <- hess(theta,...)
+  
+  #check Hessian and recalculate hessian by finite differencing if has no Hessian attribute
   if (any(is.null(H))) {
-    H <- matrix(0L, nrow = length(theta), ncol = length(theta))
+    #create a new matrix for Hessian
+    H <- matrix(0, nrow = length(theta), ncol = length(theta))
     for (i in 1:length(theta)){
       th1 <- theta; th1[i] <- th1[i]+eps
       H <- grad(th1,...)
-      H[,i] <- (H - gradient)/eps
+      H[i,] <- (H - gradient)/eps
     }
-    H <-(t(H) + H)/2
+    #make Hessian matrix symmetric
+    H <- 0.5 * (t(H) + H)
   }
-
+  
+  #check objective and derivatives are whether nor not finite
   if(is.finite(f)==FALSE || any(is.finite(gradient))==FALSE || any(is.finite(H))==FALSE){
     stop("The objective or derivatives are not finite at the initial theta.")
   }
+  
   iter <- 0
-  while(iter<maxit){
+  while(iter<=maxit){
+    #convergence check
     while(max(abs(gradient)) > tol*(abs(f)+fscale)){
       per_iter <- 0
       #perturb Hessian if Hessian is not positive definite
       while(isTRUE(inherits(try(chol(H), silent = TRUE), "try-error"))){
         I <- diag(nrow = nrow(H))
         H <- H + I*norm(H)*eps*10^per_iter
-        
-        #I <- diag(abs(max(H))*10^per_iter*eps, nrow=nrow(H), ncol=ncol(H))
-        #H <- H + I
         per_iter <- per_iter + 1
       }
-      # Calculate forward step towards optimum (minimum)
+      #calculate Delta
       Delta <- t(chol2inv(chol(H))%*%(-gradient))
-      #Delta <- backsolve(chol(H), forwardsolve(t(chol(H)), -gradient))
-      #Delta <- Delta(H,gradient)
-      #try the max.half step halvings
+      
+      #try the max.half step halving
       half <- 0
-      while(is.finite(func(theta+Delta),...)==FALSE || any(is.finite(grad(theta+Delta),...))==FALSE ||any(is.finite(hess(theta+Delta),...))==FALSE){
+      while (func(theta + Delta, ...)[1] > f[1]){
         if(half < max.half){
           Delta <- Delta/2
           half <- half + 1
         }else{
-          stop("The step fails to reduce the objective despite trying 20 steps halvings.")
+          stop("The step fails to reduce the objective despite trying 20 halvings.")
         }
       }
-      #renew theta, f, gradient and hessian
+      
+      #renew theta, f, gradient and Hessian
       theta <- theta + Delta
       f <- func(theta, ...)
       gradient <- grad(theta, ...)
       H <- hess(theta, ...)
       
-      #check renew Hessian
+      #check renew Hessian and recalculate Hessian if has no Hessian attribute
       if (any(is.null(H))) {
-        H <- matrix(0L, nrow = length(theta), ncol = length(theta))
+        #create a new matrix for Hessian
+        H <- matrix(0, nrow = length(theta), ncol = length(theta))
         for (i in 1:length(theta)){
           th1 <- theta; th1[i] <- th1[i]+eps
           H <- grad(th1,...)
-          H[,i] <- (H - gradient)/eps
+          H[i,] <- (H - gradient)/eps
         }
+        #make Hessian matrix symmetric
         H <- 0.5 * (t(H) + H)
       }
       iter <- iter + 1
     }
-    if (max(abs(gradient)) < (abs(f)+fscale)*tol){
-      if (isTRUE(class(try(chol(H),silent=TRUE)) == "try-error")){
-        print("The Hessian is not positive definite at convergence")
-      }else{
-        Hi <- chol2inv(chol(H))
-        newton_list1 <- list(f=f, theta=theta, iter=iter, g=gradient, Hi=Hi)
-        return(newton_list1)
-      } 
-    }
+    
+    #check convergence while outside while loop
+    #check whether or not Hessian is positive definite at convergence
+    if (isTRUE(class(try(chol(H),silent=TRUE)) == "try-error")){
+      warning("The Hessian is not positive definite at convergence.")  
+    }else{
+      #inverse Hessian
+      Hi <- chol2inv(chol(H))
+      newton_list <- list(f=f, theta=theta, iter=iter, g=gradient, Hi=Hi)
+      return(newton_list)
+    } 
   }
-  stop("Maximum 100 iterations is reached without convergence.")
+  #try the maximum iteration
+  warning("Maximum 100 iterations is reached without convergence.")
 }
-
-#test function
-rb <- function(th,k=2) {
-  k*(th[2]-th[1]^2)^2 + (1-th[1])^2
-}
-
-
-gb <-  function(th,k=2) {
-  c(-2*(1-th[1])-k*4*th[1]*(th[2]-th[1]^2),k*2*(th[2]-th[1]^2))
-}
-
-hb <- function(th,k=2) {
-  h <- matrix(0,2,2)
-  h[1,1] <- 2-k*2*(2*(th[2]-th[1]^2) - 4*th[1]^2)
-  h[2,2] <- 2*k
-  h[1,2] <- h[2,1] <- -4*k*th[1]
-  h
-}
-
-
-Output1 <- newt(c(1, 1),rb,gb,hb)
-Output2<-newt(c(40,3.14),rb,gb,hb)
-
-Output3<-newt(c(20,20),rb,gb,hb)
-Output4<-newt(c(1.037, 1.076),rb,gb,hb)
 
